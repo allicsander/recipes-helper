cf22489981b20ea82556ce34dbe1f3b84e8db54f
allicsander <me@evergoal.ru>
2020-02-22 14:50:04 +0300

    search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,6 @@
- import axios from 'axios';
- import {proxy, app_id, app_key} from './models/Search';
+import Search from './models/Search';
 
+const search = new Search('beans');
 
-async function getResults(query){
-    try{
-        const res = await axios(`${proxy}https://api.edamam.com/search?q=${query}&app_id=${app_id}&app_key=${app_key}&from=0&to=3&calories=591-722&health=alcohol-free`);
-
-        const recipes = res.data.hits;
-        console.log(recipes);
-    }catch(error){
-        console.log(error);
-    }
-     
-}
-
-getResults('chicken');
+console.log(search);
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,22 @@
-export default 'im sexy text here'
+import axios from 'axios';
+import {proxy, app_id, app_key} from './Config';
+
+export default class Search{
+    constructor(query){
+        this.query = query;
+
+    }
+
+    async getResults(){
+        try{
+            const res = await axios(`${proxy}https://api.edamam.com/search?q=${this.query}&app_id=${app_id}&app_key=${app_key}&from=0&to=3&calories=591-722&health=alcohol-free`);
+    
+            this.recipes = res.data.hits;
+            console.log(this.recipes);
+        }catch(error){
+            console.log(error);
+        }
+         
+    }
+
+}
