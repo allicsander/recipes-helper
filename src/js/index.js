46ce9f6da03aec1c1430a9a9c66784d2059299fe
allicsander <me@evergoal.ru>
2020-02-22 20:20:45 +0300

    updated search view

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,28 @@
 import Search from './models/Search';
-
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 
 const state = {};
 const controlSearch = async () => {
-    const query = 'beans'; // TODO view
+    const query = searchView.getInput();
+    console.log(query);
 
     if(query){
         state.search = new Search(query);
 
+        searchView.clearInput();
+        searchView.clearResults();
+
         await state.search.getResults();
         console.log("yummy yummy");
         console.log(state.search.recipes);
+
+        searchView.renderResults(state.search.recipes);
     }
 };
 
-document.querySelector('.search').addEventListener('submit', e =>{
+elements.searchForm.addEventListener('submit', e =>{
     e.preventDefault();
     controlSearch();
 });
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,3 +1,36 @@
-export const add = (a, b) => a+b;
-export const multy = (a, b) => a*b;
-export const ID = 3;
+import { elements } from './base';
+
+export const getInput = () => elements.searchInput.value;
+
+export const clearInput = () => {
+    elements.searchInput.value = '';
+};
+
+export const clearResults = () =>{
+    elements.searchResultList.innerHTML = '';
+};
+
+const renderRecipe = recipe =>{
+    
+    const markup =`
+    <li>
+        <a class="results__link" href="#${recipe.recipe.uri}">
+            <figure class="results__fig">
+                <img src="${recipe.recipe.image}" alt="Test">
+            </figure>
+            <div class="results__data">
+                <h4 class="results__name">${recipe.recipe.label}</h4>
+                <p class="results__author">${recipe.recipe.source}</p>
+            </div>
+        </a>
+    </li>
+    `;
+    console.log(markup);
+    elements.searchResultList.insertAdjacentHTML('beforeend', markup);
+
+};
+
+export const renderResults = recipes =>{
+    console.log(recipes);
+    recipes.forEach(renderRecipe);
+};
