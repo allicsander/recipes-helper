e75cebf0a5c825590cdc9b0c11ca297d070012ed
allicsander <me@evergoal.ru>
2020-02-22 10:42:17 +0300

    testing edamam.com api

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
-import sexyString from './models/Search';
-import {add as a, multi as m, ID} from './views/searchView';
+ import axios from 'axios';
+ import {proxy, app_id, app_key} from './models/Search';
 
-console.log(`using imported ${a(ID, 5)}`);
+
+async function getResults(query){
+    try{
+        const res = await axios(`${proxy}https://api.edamam.com/search?q=${query}&app_id=${app_id}&app_key=${app_key}&from=0&to=3&calories=591-722&health=alcohol-free`);
+
+        const recipes = res.data.hits;
+        console.log(recipes);
+    }catch(error){
+        console.log(error);
+    }
+     
+}
+
+getResults('chicken');
